d26accf3f3cde0d31f851d9b9a447a4b393c6c40
Richard Barnes <rbarnes23@gmail.com>
2022-02-16 10:20:28 -0500

    Update MotorController.h
    Modified various variables

--- src/MotorController.h
+++ src/MotorController.h
@@ -2,10 +2,9 @@
  * @ Author: Richard Barnes
  * @ Create Time: 2022-02-12 09:59:40
  * @ Modified by: Richard Barnes
- * @ Modified time: 2022-02-13 01:46:05
+ * @ Modified time: 2022-02-16 10:15:55
  * @ Description:
  */
-
 
 #ifdef __MOTORCONTROLLER_H__
 #define __MOTORCONTROLLER_H__
@@ -16,7 +15,7 @@
 #else
 #include "WProgram.h"
 #endif
-
+#define ESP32M true
 //#define DEBUG true
 // vra and vrb are used to adjust individual motor speeds on the fly
 struct motorDrive
@@ -35,8 +34,7 @@ struct PWM
     float right_voltage;
 };
 
-//enum BoardType{NODEMCU32S,TEENSY40};
-
+// enum BoardType{NODEMCU32S,TEENSY40};
 
 class Motor
 {
@@ -44,6 +42,8 @@ public:
     motorDrive calculateMotorSpeeds(uint16_t pwm_X, uint16_t pwm_Y, uint8_t resolution = 8, uint8_t min_left = 45, uint8_t min_right = 45);
     motorDrive calculateTurn(int16_t nJoyX, int16_t nJoyY, uint16_t dead_zone);
     float fmap(float sensorValue, float sensorMin, float sensorMax, float outMin, float outMax);
+
+
 protected:
 private:
 };
@@ -72,7 +72,6 @@ private:
     int16_t _frequency;
     int16_t _resolution;
     uint8_t _motor_no;
-    uint8_t _board_no;
 
     // Setting PWM properties
     uint8_t _pwmChannel_LEFT;
@@ -87,7 +86,7 @@ public:
     // Variables
 
     // Methods
-    void init(uint8_t motor_no, uint8_t motor_EN_L_pin, uint8_t motor_EN_R_pin, uint8_t motor_PWM_L_pin, uint8_t motor_PWM_R_pin, uint16_t frequency, uint16_t resolution, int flip_dir);
+    void init(uint8_t motor_no, uint8_t motor_BRAKE_pin, uint8_t motor_DIRECTION_pin, uint8_t motor_PWM_pin, uint8_t motor_ENCODER_pin, uint16_t frequency, uint16_t resolution, int flip_dir);
     void drive(int8_t dir, uint16_t motorSpeed);
     void motorEnable(int pin, int hilo);
     void stop();
@@ -97,26 +96,17 @@ protected:
 private:
     // Variables
     int8_t _flip_dir = 1;
-    uint8_t _motor_EN_L_pin;
-    uint8_t _motor_EN_R_pin;
-    uint8_t _motor_PWM_L_pin;
-    uint8_t _motor_PWM_R_pin;
-    int16_t _frequency;
-    int16_t _resolution;
-    uint8_t _motor_no;
-    uint8_t _board_no;
-
-    // Setting PWM properties
-    uint8_t _pwmChannel_LEFT;
-    uint8_t _pwmChannel_RIGHT;
-
-    // RIO RAND BRUSHLESS 6 to 60 volt
-    int8_t _last_direction;
     uint8_t _motor_BRAKE_pin;
     uint8_t _motor_DIRECTION_pin;
     uint8_t _motor_PWM_pin;
-    uint8_t _encoder_pin;
-    volatile int posi;
+    uint8_t _motor_ENCODER_pin;
+    int16_t _frequency;
+    int16_t _resolution;
+    uint8_t _motor_no;
+
+    // RIO RAND BRUSHLESS 6 to 60 volt
+    int8_t _last_direction;
+    volatile int _posi;
     // Methods
 };
 
